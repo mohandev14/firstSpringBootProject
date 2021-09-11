525f0a54fc7e23a73c1ea7fe04c8391debb3ddb8
mohandev14 <90524371+mohandev14@users.noreply.github.com>
2021-09-11 23:19:16 +0530

    Add files via upload

---
+++ spring-basic-security/src/main/java/com/java/security/controller/ApplicationController.java
@@ -0,0 +1,17 @@
+package com.java.security.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/rest/auth")
+public class ApplicationController {
+	@GetMapping("/getMsg")
+	 public String welcome()
+	 {
+		return "security cheak";
+		 
+	 }
+
+}
---
+++ spring-basic-security/src/test/java/com/java/security/SpringBasicSecurityApplicationTests.java
@@ -0,0 +1,12 @@
+package com.java.security;
+
+import org.junit.jupiter.api.Test;
+
+//@SpringBootTest
+class SpringBasicSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
